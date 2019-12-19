ec978a608f5f110d74488468b2dc73e53b0a2645
Alloco25 <amanehosanna@gmail.com>
2019-12-19 05:04:37 +0000

    added exercice 9 E13

--- quizz3.20.sql
+++ quizz3.20.sql
@@ -416,13 +416,111 @@ where
 
 ---------------------------
 -- Exercice 8 E13
-
 -- titre
--- contenu
--- enonce
+securityBreach -- contenu
+Vous gérez un grand site Web qui utilise un algorithme spécial pour l 'identification des utilisateurs, en particulier, il génère un attribut unique pour chaque personne en fonction uniquement de son prénom et de son nom et de certaines métadonnées supplémentaires.
+
+Aujourd' hui,
+après avoir analysé les journaux du serveur,
+vous avez découvert que la sécurité du site Web avait été violée et que les données de certains de vos utilisateurs pouvaient avoir été compromises.Les informations des utilisateurs sont stockées dans la table des utilisateurs avec la structure suivante: --
+--
+prenom: prénom de l 'utilisateur;
+prenom: nom de famille de l' utilisateur;
+
+attribut: une chaîne d 'attribut unique de cet utilisateur.' --
+--
+Il semble que seuls les utilisateurs dont l ' attribut a été généré par l ' ancienne version de votre algorithme spécial ont été affectés.> % < zéro ou plusieurs caractères arbitraires >.Il est maintenant de votre devoir d ' avertir' les utilisateurs qui ont ces attributs des risques possibles.Compte tenu de la table des utilisateurs,
+composez la table résultante composée uniquement des lignes qui contiennent les informations des utilisateurs concernés.Le résultat doit être trié par les attributs dans l 'ordre croissant.' -- enonce
 -- sql creation
+--
+--
+nom prenom attribut Mikel Cover % Mikel_Cover % Vicenta Kravitz 0 % Vicenta_Kravitz % Tosha Cover 02VO1aJ767GF7L186lpIfBR0fQ5406Q02YcpG42LDF4Bv26 Shayne Dahlquist 0R0V331K8Q7ypBi4Az3B6Nm0jCqUk % Shayne_Dahlquist % 46E3O0u7t7 Carrol Llanes 2mDIb1SdJne5wfH1Al32BE92r7j1d60PJ263b2vyPn3zxQ2P7sVOM26J11UT6W0Np Lizabeth Cover d1gM87S0NEHp386jXOk0aDc7w8bx4u8q7D82ff2Z4YT43iLyZ39xYbEDXMk Mack Chace fAnU49nMrmGu308627J7ne3qqqSPJDnq6dwW607lahNB5DinTR2Rkp549G7 Vicenta Marchese kUJ3N67vLB07mQL9Ai7p18cXGzjdT32r8283ZQi Mikel Kravitz PBX86iw1Ied87Z9OarE6sdSLdt % Mikel_Kravitz % W73XOY9YaOgi060r2x12D2EmD7 Deirdre Chace PBX86iw1Ied87Z9OarE6sdSLdtDeirdrelChaceW73XOY9YaOgi060r2x12D2EmD7 Josphine Arzate PwWD95BCKVYn5YD7iHLMa3HjP9tH % josphine_arzate % d2hNHNd3RpqfUREN47 Deirdre Chace ryCE5FIyS8q54A5036luzVS91j6C7P76E9X0O58htzgthuX24LG % DEirdre_Chace % --
+--
+create table users(
+    id int primary key,
+    nom varchar(50),
+    prenom varchar(50),
+    attribut varchar(255)
+)
+insert into
+    users(nom, prenom, attribut)
+values
+    ('Mikel', 'Cover', '%Mikel_Cover%'),
+    ('Vicenta', 'Kravitz', '0%Vicenta_Kravitz%'),
+    (
+        'Tosha',
+        'Cover',
+        '02VO1aJ767GF7L186lpIfBR0fQ5406Q02YcpG42LDF4Bv26'
+    ),
+    (
+        'Shayne',
+        'Dahlquist',
+        '0R0V331K8Q7ypBi4Az3B6Nm0jCqUk%Shayne_Dahlquist%46E3O0u7t7'
+    ),
+    (
+        'Carrol',
+        'Llanes',
+        '2mDIb1SdJne5wfH1Al32BE92r7j1d60PJ263b2vyPn3zxQ2P7sVOM26J11UT6W0Np'
+    ),
+    (
+        'Lizabeth',
+        'Cover',
+        'd1gM87S0NEHp386jXOk0aDc7w8bx4u8q7D82ff2Z4YT43iLyZ39xYbEDXMk'
+    ),
+    (
+        'Mack',
+        'Chace',
+        'fAnU49nMrmGu308627J7ne3qqqSPJDnq6dwW607lahNB5DinTR2Rkp549G7',
+        'Vicenta',
+        'Marchese',
+        'kUJ3N67vLB07mQL9Ai7p18cXGzjdT32r8283ZQi'
+    ),
+    (
+        'Mikel',
+        'Kravitz',
+        'PBX86iw1Ied87Z9OarE6sdSLdt%Mikel_Kravitz%W73XOY9YaOgi060r2x12D2EmD7'
+    ),
+    (
+        'Deirdre',
+        'Chace',
+        'PBX86iw1Ied87Z9OarE6sdSLdtDeirdrelChaceW73XOY9YaOgi060r2x12D2EmD7'
+    ),
+    (
+        'Josphine',
+        'Arzate',
+        'PwWD95BCKVYn5YD7iHLMa3HjP9tH%josphine_arzate%d2hNHNd3RpqfUREN47'
+    ),
+    (
+        'Deirdre',
+        'Chace',
+        'ryCE5FIyS8q54A5036luzVS91j6C7P76E9X0O58htzgthuX24LG%DEirdre_Chace%'
+    );
+
 -- sql depart
+select
+from
+where
+order by
+;
+
 -- sql reponse
+select
+    nom,
+    prenom,
+    attribut
+from
+    users
+where
+    attribut REGEXP CONCAT(
+        '.%',
+        BINARY(nom),
+        '_',
+        BINARY(prenom),
+        '%'
+    )
+order by
+    attribut;
+
 ---------------------------
 -- Exercice 9 E14
 -- titre
